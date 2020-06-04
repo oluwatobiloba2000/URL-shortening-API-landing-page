3fecb4f6cc2e105b1a7638546c94e1a5a20d3322
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-06-04 01:03:57 +0100

    built the mid head

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './components/nav';
+import headerSvg from './images/illustration-working.svg';
 import './App.css';
 
 function App() {
@@ -7,6 +8,16 @@ function App() {
     <div className="App">
       <div className="container bg-white">
         <Nav/>
+        <div className="mid-header">
+          <div className="introductory-text-container">
+            <h1>More than just shorter links</h1>
+            <h5>Build your brand’s recognition and get detailed insights on how your links are performing.</h5>
+            <button>Get Started</button>
+          </div>
+          <div className="home-svg">
+            <img src={headerSvg} alt="working illustration"/>
+          </div>
+        </div>
       </div>
     </div>
   );
